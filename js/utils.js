bba35ee450a370089d0325994ad051bc4095e906
Dan Sullivan <daniel.f.sullivan@gmail.com>
2026-01-06 20:40:01 -0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,10 @@ export function getQueryParam(name) {
     const params = new URLSearchParams(window.location.search);
     return params.get(name);
 }
+
+
+function setTextAll(selector, text) {
+  document.querySelectorAll(selector).forEach(el => {
+    el.textContent = text ?? '';
+  });
+}
